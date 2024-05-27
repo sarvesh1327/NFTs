36fd731a6d811278584f4349c1026665be1a13e3
Sarvesh <sarveshagl1327@gmail.com>
2024-05-27 15:36:59 +0530

    initiated dynamic NFT

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.8.20 ;
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
 import {SimpleNFT} from "../src/SimpleNFT.sol";
+import {DynamicNFT} from "../src/DynamicNFT.sol";
 
 contract MintSimpleNFT is Script {
         string public constant pug = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json"; 
@@ -17,6 +18,19 @@ contract MintSimpleNFT is Script {
 
     function run() external {
         address mostRecentlyDeploy = DevOpsTools.get_most_recent_deployment("SimpleNFT", block.chainid);
+        mintNFTOnContract(mostRecentlyDeploy);
+    }
+}
+
+contract MintDynamicNFT is Script {
+    function mintNFTOnContract(address mostRecentlyDeploy) public{
+        vm.startBroadcast();
+        DynamicNFT(mostRecentlyDeploy).mint();
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentlyDeploy = DevOpsTools.get_most_recent_deployment("DynamicNFT", block.chainid);
         mintNFTOnContract(mostRecentlyDeploy);
     }
 }
---
+++ src/DynamicNFT.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DynamicNFT is ERC721 {
+    uint256 private s_tokenCounter;
+    string private s_happySvgImageUri;
+    string private s_sadSvgImageUri;
+
+    enum MOOD {
+        SAD,
+        HAPPY
+    }
+
+    mapping(uint256 => MOOD) private s_tokenIdToMood;
+
+    constructor(
+        string memory happySvgImageUri,
+        string memory sadSvgImageUri
+    ) ERC721("Dynamic NFT", "DNF") {
+        s_tokenCounter = 0;
+        s_happySvgImageUri = happySvgImageUri;
+        s_sadSvgImageUri = sadSvgImageUri;
+    }
+
+    function mint() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = MOOD.HAPPY;
+        s_tokenCounter += 1;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64, ";
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        string memory imageUri = s_tokenIdToMood[tokenId] == MOOD.HAPPY
+            ? s_happySvgImageUri
+            : s_sadSvgImageUri;
+        bytes memory data = bytes(
+            abi.encodePacked(
+                '{"name": "',
+                name(),
+                '", "description": "An NFT that reflects owners mood.", "attributes": [{"trait_type": "moodiness", "value": "100"}], "image": "',
+                imageUri,
+                '"}'
+            )
+        );
+        return string(abi.encodePacked(_baseURI(), Base64.encode(data)));
+    }
+}
